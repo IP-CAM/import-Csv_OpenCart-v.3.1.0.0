d779502eca4dd7322ac1fb6a8c7d977e6205533a
ina <ina.kacidhe@serijakala.com>
2018-04-10 10:10:15 +0200

    First Commit

---
+++ admin/language/en-gb/common/header.php
@@ -0,0 +1,17 @@
+<?php
+// Heading
+$_['heading_title']      = 'MySHop';
+
+// Text
+$_['text_profile']       = 'Your Profile';
+$_['text_store']         = 'Stores';
+$_['text_help']          = 'Help';
+$_['text_homepage']      = 'OpenCart Homepage';
+$_['text_support']       = 'Support Forum';
+$_['text_documentation'] = 'Documentation';
+$_['text_logout']        = 'Logout';
+$_['text_my_module']     = 'First Module';
+
+
+
+$_['futja_kottttt']      = 'futja_kottttt';
